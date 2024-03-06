647ddca7d949cb2936e0ff0ec50ce5192722d2fc
LinThit27 <linthit2745@gmail.com>
2024-03-06 10:12:26 +0630

    initial commit

---
+++ Easy/maximumOddBinaryNumber.js
@@ -0,0 +1,58 @@
+// https://leetcode.com/problems/maximum-odd-binary-number/description/
+
+// You are given a binary string s that contains at least one '1'.
+// You have to rearrange the bits in such a way that the resulting
+// binary number is the maximum odd binary number that can be created from this combination.
+// Return a string representing the maximum odd binary number that
+// can be created from the given combination.
+// Note that the resulting string can have leading zeros.
+
+//-------------------------------------------------
+
+// Example 1:
+
+// Input: s = "010"
+// Output: "001"
+// Explanation: Because there is just one '1',
+// it must be in the last position. So the answer is "001".
+
+// Example 2:
+
+// Input: s = "0101"
+// Output: "1001"
+// Explanation: One of the '1's must be in the last position.
+// The maximum number that can be made with the remaining digits is "100".
+// So the answer is "1001".
+
+//Solution
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var maximumOddBinaryNumber = function (s) {
+  let charArr = s.split("").sort().reverse();
+  for (let i = charArr.length - 1; i >= 0; i--) {
+    if (charArr[i] === "1") {
+      [charArr[i], charArr[charArr.length - 1]] = [
+        charArr[charArr.length - 1],
+        charArr[i],
+      ];
+      break;
+    }
+  }
+  return charArr.join("");
+};
+
+// Explanation
+
+// First Convert the binary to an Array
+// After that sort them and reverse  it
+// for example, 100110 will become [1,1,1,0,0,0]
+// and then loop them from end to start
+// Once You spot the 1 , swap it with the last zero
+// in this case
+// it will become [1,1,0,0,0,1]
+
+// finally join the array back to form a string
+// returning 110001 as an answer
